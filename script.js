395d524a0a5ae85626c4a5c1536078ae9a5c367f
haydenlinder <haydenlinder@gmail.com>
2022-09-03 12:39:25 -0700

    Done

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ import { OBJLoader } from 'OBJLoader';
  *  
  */
 
-export const renderModels = (models/** Models Type */) => models.map(({ title, filePath }) => {
+export const renderModels = (models/** Models Type */) => models.map(({ title, filePath, scale }) => {
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+    var camera = new THREE.PerspectiveCamera(75, (window.innerWidth*0.8) / (window.innerHeight*0.8));
 
     var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth*0.8, window.innerHeight*0.8);
     const titleElement = document.createElement('h1')
     titleElement.innerText = title
     document.body.append(titleElement);
@@ -34,17 +34,15 @@ export const renderModels = (models/** Models Type */) => models.map(({ title, f
     controls.enableDamping = true;
     controls.dampingFactor = 0.05;
 
-    animate();
-
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+    
 
     const light = new THREE.AmbientLight()
     scene.add(light)
 
+    let savedObject = { rotation: { x: 0, y: 0, z: 0 } };
+
     const loader = new OBJLoader();
     loader.load(filePath, function (object) {
-        let scale = 10;
         object.traverse(function (child) {
             if (child instanceof THREE.Mesh) {
                 child.geometry.center();
@@ -56,25 +54,28 @@ export const renderModels = (models/** Models Type */) => models.map(({ title, f
                 child.castShadow = true;
             }
         });
-
+        // object.rotation.y = 1
         object.scale.divideScalar(scale);
-
+        savedObject = object
         scene.add(object)
     });
 
     window.addEventListener('resize', onWindowResize);
 
     function onWindowResize() {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
+        const width = window.innerWidth*0.8;
+        const height = window.innerHeight*0.8;
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
         renderer.setSize(width, height);
     }
 
     function animate() {
+        savedObject.rotation.z += 0.01
+        savedObject.rotation.x += 0.005
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
         controls.update();
     }
+    animate();
 })
